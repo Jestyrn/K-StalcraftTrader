41ba455658688173826607c178d083a2bbb5a1df
mowech <mr9rey@yandex.ru>
2025-04-19 00:49:49 +0400

    #добавлены страницы для скупки, и настройки

--- TraderForStalCraft/Form1.Designer.cs
+++ TraderForStalCraft/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            TabControll = new TabControl();
+            MainPage = new TabPage();
+            tabPage2 = new TabPage();
+            TabControll.SuspendLayout();
+            SuspendLayout();
+            // 
+            // TabControll
+            // 
+            TabControll.Controls.Add(MainPage);
+            TabControll.Controls.Add(tabPage2);
+            TabControll.Dock = DockStyle.Fill;
+            TabControll.Location = new Point(0, 0);
+            TabControll.Name = "TabControll";
+            TabControll.SelectedIndex = 0;
+            TabControll.Size = new Size(800, 450);
+            TabControll.TabIndex = 0;
+            // 
+            // MainPage
+            // 
+            MainPage.Location = new Point(4, 24);
+            MainPage.Name = "MainPage";
+            MainPage.Padding = new Padding(3);
+            MainPage.Size = new Size(792, 422);
+            MainPage.TabIndex = 0;
+            MainPage.Text = "Основное";
+            MainPage.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 422);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TabControll);
+            Name = "Form1";
+            Text = "Form1";
+            TabControll.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl TabControll;
+        private TabPage MainPage;
+        private TabPage tabPage2;
     }
 }
